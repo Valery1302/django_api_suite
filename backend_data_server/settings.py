149d8444b00da776f3d6cf3525e4329dfdda0da2
Valeria Gutierrez <142460056+Valery1302@users.noreply.github.com>
2026-01-19 04:24:37 +0000

    fix middleware

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -154,7 +154,13 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    # ...
+
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 STORAGES = {
